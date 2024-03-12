b54978d882e3e4882902df173ae4608eef58d49e
johnLamberts <faithsacredoo3@gmail.com>
2024-03-12 23:15:06 +0800

    fixed: responsive filter to library page

--- school-library-app/src/features/HomePage/Library/LibraryFilter.tsx
+++ school-library-app/src/features/HomePage/Library/LibraryFilter.tsx
@@ -1,17 +1,29 @@
-import { Accordion, Paper, Title } from "@mantine/core";
+import { Accordion, Burger, Button, Drawer, Paper, Title } from "@mantine/core";
 import BookFilter from "./BookFilter";
 import { useReadBookType } from "@features/SysSettings/BookType/hooks/useReadBookType";
 import { useReadGenre } from "@features/SysSettings/BookGenre/hooks/useReadGenre";
+import { useDisclosure } from "@mantine/hooks";
+import { useSearchParams } from "react-router-dom";
 
 const LibraryFilter = () => {
   const { data: bookTypes = [], isLoading: isBookTypeLoading } =
     useReadBookType();
   const { data: genresData = [], isLoading: isGenresLoading } = useReadGenre();
+  const [opened, { close, toggle }] = useDisclosure(false);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleResetFilterField = () => {
+    if (searchParams.get("genre") !== "") searchParams.delete("genre");
+
+    if (searchParams.get("bookType") !== "") searchParams.delete("bookType");
+
+    return setSearchParams(searchParams);
+  };
   if (isBookTypeLoading || isGenresLoading) return <>Loading...</>;
   return (
     <>
-      <Paper shadow="xs" p={"xl"}>
+      <Paper shadow="xs" p={"xl"} visibleFrom="md">
         <Title order={4}>Filter</Title>
 
         <Accordion defaultValue={["Academic", "Genres"]} multiple>
@@ -44,6 +56,63 @@ const LibraryFilter = () => {
           </Accordion.Item>
         </Accordion>
       </Paper>
+
+      <Burger
+        opened={opened}
+        onClick={toggle}
+        size="md"
+        hiddenFrom="md"
+        color="#5c0505"
+      />
+
+      {searchParams.get("genre") !== "" ||
+      searchParams.get("bookType") !== "" ? (
+        <Button
+          variant="outline"
+          onClick={handleResetFilterField}
+          size="xs"
+          hiddenFrom="md"
+          color="#5c0505"
+        >
+          Clear filter
+        </Button>
+      ) : null}
+
+      <Drawer opened={opened} onClose={close} position="left" size={"xs"}>
+        <Paper shadow="xs" p={"xl"} hiddenFrom="md">
+          <Title order={4}>Filter</Title>
+
+          <Accordion defaultValue={["Academic", "Genres"]} multiple>
+            <Accordion.Item value="Academic">
+              <Accordion.Control c={"#5C0505"}>Academic</Accordion.Control>
+              <Accordion.Panel>
+                {/* TODO: Checkbox -- Filter by Book Types */}
+
+                <BookFilter
+                  options={bookTypes.map((book) => ({
+                    label: book.bookType,
+                    value: book.bookType,
+                  }))}
+                  paramsName="bookType"
+                />
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item value="Genres">
+              <Accordion.Control c={"#5C0505"}>Topics</Accordion.Control>
+              <Accordion.Panel>
+                <BookFilter
+                  options={genresData.map((book) => ({
+                    label: book.genres,
+                    value: book.genres,
+                  }))}
+                  paramsName="genre"
+                />
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </Paper>
+      </Drawer>
     </>
   );
 };
