004c525cd59b8c53bc3d84dca34ce13d513b5949
johnLamberts <faithsacredoo3@gmail.com>
2024-03-17 14:46:35 +0800

    fixed: node_moduels outside

--- school-library-app/src/features/Announcement/AnnouncementForm.tsx
+++ school-library-app/src/features/Announcement/AnnouncementForm.tsx
@@ -1,4 +1,39 @@
-const AnnouncementForm = () => {
-  return <div>AnnouncementForm</div>;
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+const SAMPLE_READ_ONLY_LOCK_ID = "Integration Sample";
+
+type EditorDemoProps = {
+  content: string;
 };
-export default AnnouncementForm;
+
+type EditorDemoState = {
+  documents: Array<string>;
+  documentID: number;
+  editor: ClassicEditor | null;
+};
+
+export default function AnnouncementForm() {
+  return (
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        onReady={() => {}}
+        config={{
+          simpleUpload: {
+            uploadUrl: "https://myserver.herokuapp.com/image-upload",
+          },
+        }}
+        onChange={(event, editor) => {
+          console.log("event: onChange", { event, editor });
+        }}
+        onBlur={(event, editor) => {
+          console.log("event: onBlur", { event, editor });
+        }}
+        onFocus={(event, editor) => {
+          console.log("event: onFocus", { event, editor });
+        }}
+      />
+    </>
+  );
+}
--- school-library-app/vite.config.ts
+++ school-library-app/vite.config.ts
@@ -4,4 +4,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  optimizeDeps: {
+    /**
+     * This is required only because we use "npm link" for
+     * testing purposes. See `dependencies` in `package.json`.
+     */
+    include: ["@ckeditor/ckeditor5-react"],
+  },
 });
