a00f21db87a1e1389ef4d9ce9c92018e82440c59
johnLamberts <faithsacredoo3@gmail.com>
2024-03-17 15:54:48 +0800

    feat: add ck-editor and relative plugins/libraries when adding new announcement

--- school-library-app/src/features/Announcement/AnnouncementForm.tsx
+++ school-library-app/src/features/Announcement/AnnouncementForm.tsx
@@ -1,39 +1,109 @@
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-
-const SAMPLE_READ_ONLY_LOCK_ID = "Integration Sample";
-
-type EditorDemoProps = {
-  content: string;
-};
-
-type EditorDemoState = {
-  documents: Array<string>;
-  documentID: number;
-  editor: ClassicEditor | null;
-};
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+import { CKEditor, CKEditorContext } from "@ckeditor/ckeditor5-react";
+import { Context } from "@ckeditor/ckeditor5-core";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import LinkPlugin from "@ckeditor/ckeditor5-link/src/link";
+import ParagraphPlugin from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import BlockquotePlugin from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import {
+  Image,
+  ImageToolbar,
+  ImageCaption,
+  ImageStyle,
+  ImageResize,
+} from "@ckeditor/ckeditor5-image";
+import ImageUploadPlugin from "@ckeditor/ckeditor5-image/src/imageupload";
+import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
+import { Font } from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Box } from "@mantine/core";
+import { Alignment } from "@ckeditor/ckeditor5-alignment"; // Importing the package.
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
 
 export default function AnnouncementForm() {
   return (
     <>
-      <CKEditor
-        editor={ClassicEditor}
-        onReady={() => {}}
-        config={{
-          simpleUpload: {
-            uploadUrl: "https://myserver.herokuapp.com/image-upload",
-          },
-        }}
-        onChange={(event, editor) => {
-          console.log("event: onChange", { event, editor });
-        }}
-        onBlur={(event, editor) => {
-          console.log("event: onBlur", { event, editor });
-        }}
-        onFocus={(event, editor) => {
-          console.log("event: onFocus", { event, editor });
-        }}
-      />
+      <Box h={"60rem"}>
+        <CKEditorContext context={Context}>
+          <CKEditor
+            config={{
+              plugins: [
+                Base64UploadAdapter,
+                LinkPlugin,
+                ParagraphPlugin,
+                BlockquotePlugin,
+                Image,
+                ImageCaption,
+                ImageResize,
+                ImageStyle,
+                ImageToolbar,
+                ImageUploadPlugin,
+                Bold,
+                Italic,
+                Essentials,
+                Font,
+                Heading,
+                Alignment,
+                TextTransformation,
+              ],
+              image: {
+                toolbar: [
+                  "imageTextAlternative",
+                  "toggleImageCaption",
+                  "imageStyle:inline",
+                  "imageStyle:block",
+                  "imageStyle:side",
+                ],
+              },
+              toolbar: {
+                items: [
+                  "undo",
+                  "redo",
+                  "|",
+                  "alignment", //
+                  "heading",
+                  "|",
+                  "fontfamily",
+                  "fontsize",
+                  "fontColor",
+                  "fontBackgroundColor",
+                  "|",
+                  "bold",
+                  "italic",
+                  "strikethrough",
+                  "subscript",
+                  "superscript",
+                  "code",
+                  "|",
+                  "link",
+                  "insertImage",
+                  "blockQuote",
+                  "codeBlock",
+                  "|",
+                  "bulletedList",
+                  "numberedList",
+                  "todoList",
+                  "outdent",
+                  "indent",
+                ],
+                shouldNotGroupWhenFull: false,
+              },
+            }}
+            editor={ClassicEditor}
+            onReady={() => {}}
+            onChange={(event, editor) => {
+              console.log("event: onChange", { event, editor });
+            }}
+            onBlur={(event, editor) => {
+              console.log("event: onBlur", { event, editor });
+            }}
+            onFocus={(event, editor) => {
+              console.log("event: onFocus", { event, editor });
+            }}
+          />
+        </CKEditorContext>{" "}
+      </Box>
     </>
   );
 }
--- school-library-app/vite.config.ts
+++ school-library-app/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { createRequire } from "node:module";
 
+import tsconfigPaths from "vite-tsconfig-paths";
+const require = createRequire(import.meta.url);
+
+import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") }),
+  ],
   optimizeDeps: {
     /**
      * This is required only because we use "npm link" for
