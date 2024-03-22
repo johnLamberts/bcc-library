5f43732a84a642173a3cbdd40379ad65d05d525c
johnLamberts <faithsacredoo3@gmail.com>
2024-03-22 23:39:20 +0800

    feat: add ui changes to sotckk

--- school-library-app/src/features/Users/UserForm/BasicInformationForm.tsx
+++ school-library-app/src/features/Users/UserForm/BasicInformationForm.tsx
@@ -1,11 +1,12 @@
 import Form from "@components/Form/Form";
-import { TextInput } from "@mantine/core";
-import { useFormContext } from "react-hook-form";
+import { InputBase } from "@mantine/core";
+import { Controller, useFormContext } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 
 const BasicInformationForm = () => {
   const {
-    register,
     formState: { errors },
+    control,
   } = useFormContext();
 
   return (
@@ -14,36 +15,64 @@ const BasicInformationForm = () => {
         <Form.Title>Basic Information</Form.Title>
         <Form.Grid p={"lg"}>
           <Form.Col span={{ base: 12, md: 3, lg: 6 }}>
-            <TextInput
-              label="First Name"
-              placeholder="Your First Name..."
-              withAsterisk
-              withErrorStyles={errors.firstName?.message ? true : false}
-              error={<>{errors.firstName && errors.firstName?.message}</>}
-              {...register("firstName", { required: `This field is required` })}
+            <Controller
+              name="firstName"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <InputBase
+                    component={IMaskInput}
+                    withAsterisk
+                    mask={/^[a-zA-Z]+$/}
+                    label="First Name"
+                    placeholder="John Doe..."
+                    withErrorStyles={errors.firstName?.message ? true : false}
+                    {...field}
+                    error={<>{errors.firstName?.message}</>}
+                  />
+                );
+              }}
             />
           </Form.Col>
           <Form.Col span={{ base: 12, md: 3, lg: 6 }}>
-            <TextInput
-              label="Middle Name"
-              placeholder="Your Middle Name..."
-              withAsterisk
-              withErrorStyles={errors.middleName?.message ? true : false}
-              {...register("middleName", {
-                required: `This field is required`,
-              })}
-              error={<>{errors && errors.middleName?.message}</>}
+            <Controller
+              name="middleName"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <InputBase
+                    component={IMaskInput}
+                    withAsterisk
+                    mask={/^[a-zA-Z]+$/}
+                    label="Middle Name"
+                    placeholder="Dela Fuena..."
+                    withErrorStyles={errors.middleName?.message ? true : false}
+                    {...field}
+                    error={<>{errors.middleName?.message}</>}
+                  />
+                );
+              }}
             />
           </Form.Col>
 
           <Form.Col span={{ base: 12, md: 3, lg: 12 }}>
-            <TextInput
-              label="Last Name"
-              placeholder="Your Last Name..."
-              withAsterisk
-              withErrorStyles={errors.lastName?.message ? true : false}
-              {...register("lastName", { required: `This field is required` })}
-              error={<>{errors.lastName?.message}</>}
+            <Controller
+              name="lastName"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <InputBase
+                    component={IMaskInput}
+                    withAsterisk
+                    mask={/^[a-zA-Z]+$/}
+                    label="Last Name"
+                    placeholder="Fuentes Strange..."
+                    withErrorStyles={errors.lastName?.message ? true : false}
+                    {...field}
+                    error={<>{errors.lastName?.message}</>}
+                  />
+                );
+              }}
             />
           </Form.Col>
         </Form.Grid>
