caa2292601d2b3c038c801722ddbd68701d942d1
johnLamberts <faithsacredoo3@gmail.com>
2024-03-25 17:08:35 +0800

    feat: add styles

--- school-library-app/src/main.tsx
+++ school-library-app/src/main.tsx
@@ -9,12 +9,19 @@ import "@mantine/dates/styles.css";
 import "@mantine/carousel/styles.css";
 import "mantine-react-table/styles.css";
 
+// React PDF Viewer
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
 import { MantineProvider, rem } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ModalsProvider } from "@mantine/modals";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Helmet } from "react-helmet";
+
+import { Worker } from "@react-pdf-viewer/core";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -62,7 +69,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       >
         <ModalsProvider>
           <BrowserRouter>
-            <App />
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+              <App />
+            </Worker>
           </BrowserRouter>
         </ModalsProvider>
       </MantineProvider>
