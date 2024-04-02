b7c25ca23ab9c34898bfba90bf60be57b91dc6a9
johnLamberts <faithsacredoo3@gmail.com>
2024-04-02 14:06:20 +0800

    feat: add report viewer to transaction

--- school-library-app/src/pages/Reports/TransactionReport.tsx
+++ school-library-app/src/pages/Reports/TransactionReport.tsx
@@ -1,11 +1,9 @@
-import { Box, Group } from "@mantine/core";
+import { Box } from "@mantine/core";
 import TransactionReportTable from "@features/Reports/TransactionReport";
 
 export default function TransactionReport() {
   return (
     <>
-      <Group justify="space-between"></Group>
-
       {/* List of Users */}
       <Box w={"100%"}>
         <Box my="xl">
