38fbcd78d0cedc53ba17bf56749073994313fd68
johnLamberts <faithsacredoo3@gmail.com>
2024-04-02 14:10:40 +0800

    feat: add report viewer to book condition

--- school-library-app/src/features/Reports/BookConditionReport.tsx
+++ school-library-app/src/features/Reports/BookConditionReport.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Group, Box, Button, Text, Flex, Badge } from "@mantine/core";
+import { Group, Box, Button, Text, Flex, Badge, Modal } from "@mantine/core";
 import { IconFileTypeCsv, IconFileTypePdf } from "@tabler/icons-react";
 import {
   MRT_ColumnDef,
@@ -8,7 +8,7 @@ import {
   MantineReactTable,
   useMantineReactTable,
 } from "mantine-react-table";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 import classes from "@pages/styles/user.module.css";
 import { flatten } from "src/utils/helpers/flatten";
@@ -24,6 +24,10 @@ import bccLogoPng from "src/assets/bccLogo3.png";
 import { Row } from "@tanstack/react-table";
 import { ICirculation } from "@features/Transaction/models/circulation.interface";
 import useReadBookConditionnReport from "./hooks/useBookCoditionReport";
+import { useDisclosure } from "@mantine/hooks";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { Viewer } from "@react-pdf-viewer/core";
+import FeeToolbar from "./FeeFilter/FeeToolbar";
 
 const BookConditionReportTable = () => {
   const {
@@ -32,6 +36,11 @@ const BookConditionReportTable = () => {
     isError: isLoadingUsersError,
     isFetching: isFetchingUsers,
   } = useReadBookConditionnReport();
+
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [viewPdf, setViewPdf] = useState<string>();
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   const customColumns = useMemo<MRT_ColumnDef<ICirculation>[]>(
     () => [
@@ -89,11 +98,11 @@ const BookConditionReportTable = () => {
         },
       },
       {
-        accessorFn: (originalRow) => console.log(originalRow.dateReturned),
-        //     new Date(
-        //       originalRow.dateReturned?.seconds * 1000 +
-        //         originalRow.dateReturned?.nanoseconds / 1000
-        //     ),
+        accessorFn: (originalRow) =>
+          new Date(
+            originalRow.dateReturned?.seconds * 1000 +
+              originalRow.dateReturned?.nanoseconds / 1000
+          ),
         header: "Date Created",
         filterVariant: "date-range",
         Cell: ({ row }) => {
@@ -115,22 +124,23 @@ const BookConditionReportTable = () => {
   const exportCSVFile = (csvData: Row<ICirculation>[]) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const formatData = csvData?.map((user: any) => {
-      const { firstName, lastName, middleName, email, createdAt, userRole } =
+      const { bookTitle, bookType, bookISBN, bookCondition, dateReturned } =
         user.original;
 
       const date = format(
-        new Date(createdAt.seconds * 1000 + createdAt.nanoseconds / 1000),
+        new Date(
+          dateReturned?.seconds * 1000 + dateReturned?.nanoseconds / 1000
+        ),
         "MMMM dd yyyy"
       );
 
       // const createdAt = format();
       return {
-        firstName,
-        middleName,
-        lastName,
-        email,
-        userRole,
-        createdAt: date,
+        bookTitle,
+        bookType,
+        bookISBN,
+        bookCondition,
+        dateReturned: date,
       };
     });
 
@@ -188,6 +198,7 @@ const BookConditionReportTable = () => {
     renderToolbarInternalActions: ({ table }) => {
       return (
         <Flex gap="xs" align="center">
+          <FeeToolbar table={table} />
           <MRT_ToggleGlobalFilterButton table={table} />{" "}
           <MRT_ToggleDensePaddingButton table={table} />
         </Flex>
@@ -205,7 +216,9 @@ const BookConditionReportTable = () => {
         >
           <Button
             color="yellow.7"
-            onClick={() => exportCSVFile(table.getFilteredRowModel().rows)}
+            onClick={() =>
+              exportCSVFile(table.getFilteredRowModel().rows as any)
+            }
             leftSection={<IconFileTypeCsv />}
             variant="outline"
           >
@@ -217,7 +230,7 @@ const BookConditionReportTable = () => {
             //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
             onClick={() => {
               //   console.log(table.getFilteredRowModel().rows);
-              exportToPDF(table.getFilteredRowModel().rows);
+              exportToPDF(table.getFilteredRowModel().rows as any);
             }}
             leftSection={<IconFileTypePdf />}
             variant="outline"
@@ -231,13 +244,11 @@ const BookConditionReportTable = () => {
 
   const exportToPDF = async (data: Row<ICirculation>[]) => {
     const headerNamesMapping: Record<string, string> = {
-      borrowersName: "Name",
-      borrowers: "Borrower",
-      borrowersNumber: "Borrowers Number",
-      borrowersEmail: "Email",
-      status: "Status",
       bookTitle: "Title",
-      createdAt: "Date created",
+      bookType: "Book Type",
+      bookISBN: "Book ISBN",
+      bookCondition: "bookCondition",
+      "Date Created": "Date created",
     };
 
     const headerNames = table
@@ -246,60 +257,51 @@ const BookConditionReportTable = () => {
         (col) => col.id !== "select" && col.id !== "actions" && col.id !== "id"
       )
       .filter((col) => {
-        console.log(col);
         return (
-          col.id === "borrowersName" ||
-          col.id === "borrowers" ||
-          col.id === "middleName" ||
-          col.id === "borrowersNumber" ||
-          col.id === "borrowersEmail" ||
+          col.id === "bookType" ||
+          col.id === "bookISBN" ||
+          col.id === "bookCondition" ||
           col.id === "status" ||
           col.id === "bookTitle" ||
-          col.id === "createdAt"
+          col.id === "Date Created"
         );
       })
       .map((col) => headerNamesMapping[col.id]);
 
     const formatData = data?.map((user: any) => {
-      const {
-        borrowersName,
-        borrowers,
-        borrowersNumber,
-        borrowersEmail,
-        status,
-        bookTitle,
-        createdAt,
-      } = user.original;
+      const { bookTitle, bookType, bookISBN, bookCondition, dateReturned } =
+        user.original;
 
       const date = format(
-        new Date(createdAt?.seconds * 1000 + createdAt?.nanoseconds / 1000),
+        new Date(
+          dateReturned?.seconds * 1000 + dateReturned?.nanoseconds / 1000
+        ),
         "MMMM dd yyyy"
       );
 
       // const createdAt = format();
       return {
-        borrowersName,
-        borrowers,
-        borrowersNumber,
-        borrowersEmail,
-        status,
         bookTitle,
-        createdAt: date,
+        bookType,
+        bookISBN,
+        bookCondition,
+        dateReturned: date,
       };
     });
 
+    console.log(formatData, headerNames);
+
     const doc = new jsPDF("l", "mm", "a4");
 
+    const totalPagesExp = "{total_pages_count_string}";
     const extractingValues = formatData.map((doc) => Object.values(doc));
-
-    console.log(extractingValues);
 
     (doc as jsPDF & { autoTable: autoTable }).autoTable({
       headStyles: {
         fillColor: "#77050a",
         textColor: "#fff",
       },
-      didDrawCell: () => {
+      willDrawPage: () => {
         const image = new Image();
         const logoImage = new Image();
 
@@ -307,87 +309,120 @@ const BookConditionReportTable = () => {
         logoImage.src = bccLogo;
         // doc.addImage(imageRef.current, "PNG", 50, 15 - 1, 12, 12);
 
-        doc.addImage(bccLogoPng, "PNG", 50, 15 - 1, 12, 12);
-        doc.addImage(image.src, "PNG", 160, 15 - 1, 15, 15);
+        doc.addImage(bccLogoPng, "PNG", 50, 15 - 1, 25, 25);
+        doc.addImage(image.src, "PNG", 250, 15 - 1, 25, 25);
+
+        doc.internal.scaleFactor = 3.75;
+        const docWidth = doc.internal.pageSize.width;
+        // const docHeight = doc.internal.pageSize.height;
+
+        const colorBlack = "#000000";
+        const colorGray = "#1c1c1d";
+        //starting at 15mm
+        let currentHeight = 15;
+        //var startPointRectPanel1 = currentHeight + 6;
+
+        const pdfConfig = {
+          headerTextSize: 8,
+          labelTextSize: 12,
+          fieldTextSize: 10,
+          lineHeight: 6,
+          subLineHeight: 4,
+        };
+
+        doc.setFontSize(pdfConfig.headerTextSize);
+        doc.setTextColor(colorBlack);
+
+        doc.setFontSize(pdfConfig.fieldTextSize);
+        doc.setTextColor(colorGray);
+        currentHeight += pdfConfig.subLineHeight;
+
+        doc.text(
+          `Republic of the Philippnes`,
+          docWidth / 2,
+          currentHeight - 5,
+          {
+            align: "center",
+          }
+        );
+
+        currentHeight += pdfConfig.subLineHeight;
+
+        doc.setFont("", "", "bold");
+
+        doc.text(
+          `Binangonan Catholic College`,
+          docWidth / 2,
+          currentHeight - 5,
+          {
+            align: "center",
+          }
+        );
+
+        currentHeight += pdfConfig.subLineHeight;
+
+        doc.text(`Binangonan, Rizal`, docWidth / 2, currentHeight - 5, {
+          align: "center",
+        });
+
+        currentHeight += pdfConfig.subLineHeight;
+        currentHeight += pdfConfig.subLineHeight;
+        currentHeight += pdfConfig.subLineHeight;
+        currentHeight += 2;
+        doc.setFont("", "", "bold");
+        doc.setFontSize(16);
+        doc.text(
+          `Binangonan Catholic College Books Condition Report `,
+          docWidth / 2,
+          currentHeight - 5,
+          {
+            align: "center",
+          }
+        );
+
+        currentHeight += pdfConfig.subLineHeight;
+        doc.setFontSize(pdfConfig.fieldTextSize);
+        doc.setFont("", "", "normal");
+        doc.text(
+          `${format(new Date(), "MMMM dd, yyyy")}`,
+          docWidth / 2,
+          currentHeight - 5,
+          {
+            align: "center",
+          }
+        );
       },
+
       head: [headerNames],
       body: extractingValues as RowInput[],
+      startY: 50,
+      showHead: "firstPage",
       margin: { top: 50 },
+      didDrawPage: function (data) {
+        // Footer
+        let str = "Page " + doc.getNumberOfPages();
+        // Total page number plugin only available in jspdf v1.0+
+        if (typeof doc.putTotalPages === "function") {
+          str = str + " of " + totalPagesExp;
+        }
+        doc.setFontSize(10);
+
+        // jsPDF 1.4+ uses getHeight, <1.4 uses .height
+        const pageSize = doc.internal.pageSize;
+        const pageHeight = pageSize.height
+          ? pageSize.height
+          : pageSize.getHeight();
+        doc.text(str, data.settings.margin.left, pageHeight - 10);
+      },
     });
+    if (typeof doc.putTotalPages === "function") {
+      doc.putTotalPages(totalPagesExp);
+    }
 
-    doc.internal.scaleFactor = 3.75;
-    const docWidth = doc.internal.pageSize.width;
+    console.log(headerNames, formatData);
+    setViewPdf(doc.output("datauristring"));
 
-    const colorBlack = "#000000";
-    const colorGray = "#1c1c1d";
-    let currentHeight = 15;
-
-    const pdfConfig = {
-      headerTextSize: 8,
-      labelTextSize: 12,
-      fieldTextSize: 10,
-      lineHeight: 6,
-      subLineHeight: 4,
-    };
-
-    doc.setFontSize(pdfConfig.headerTextSize);
-    doc.setTextColor(colorBlack);
-
-    doc.setFontSize(pdfConfig.fieldTextSize);
-    doc.setTextColor(colorGray);
-    currentHeight += pdfConfig.subLineHeight;
-
-    doc.text(`Republic of the Philippnes`, docWidth / 2, currentHeight - 5, {
-      align: "center",
-    });
-
-    currentHeight += pdfConfig.subLineHeight;
-
-    doc.setFont("", "", "bold");
-    // doc.text(
-
-    doc.text(`Binangonan Catholic College`, docWidth / 2, currentHeight - 5, {
-      align: "center",
-    });
-
-    currentHeight += pdfConfig.subLineHeight;
-
-    doc.text(`Binangonan, Rizal`, docWidth / 2, currentHeight - 5, {
-      align: "center",
-    });
-
-    currentHeight += pdfConfig.subLineHeight;
-    currentHeight += pdfConfig.subLineHeight;
-    currentHeight += pdfConfig.subLineHeight;
-    currentHeight += 2;
-    doc.setFont("", "", "bold");
-    doc.setFontSize(16);
-    doc.text(
-      `Binangonan Catholic College Transaction Report`,
-      docWidth / 2,
-      currentHeight - 5,
-      {
-        align: "center",
-      }
-    );
-
-    currentHeight += pdfConfig.subLineHeight;
-    doc.setFontSize(pdfConfig.fieldTextSize);
-    doc.setFont("", "", "normal");
-    doc.text(
-      `${format(new Date(), "MMMM dd, yyyy")}`,
-      docWidth / 2,
-      currentHeight - 5,
-      {
-        align: "center",
-      }
-    );
-    doc.save(
-      `Binangonan Catholic College Transaction Report - ${new Date().toLocaleDateString(
-        "en-us",
-        { year: "numeric", month: "long", day: "numeric" }
-      )}.pdf`
-    );
+    open?.();
   };
 
   return (
@@ -405,6 +440,21 @@ const BookConditionReportTable = () => {
           <MantineReactTable table={table} />
         </Box>
       </Box>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Teacher Report"
+        size="calc(100vw - 3rem)"
+      >
+        <Box>
+          <Viewer
+            plugins={[defaultLayoutPluginInstance]}
+            fileUrl={viewPdf as string}
+          />{" "}
+        </Box>
+        {/* Modal content */}
+      </Modal>
     </>
   );
 };
