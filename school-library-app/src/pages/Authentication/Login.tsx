0d78d2236d98394c1388106e3338e018695cba25
johnLamberts <faithsacredoo3@gmail.com>
2024-04-04 23:15:29 +0800

    fix: login ui optimized

--- school-library-app/src/pages/Authentication/Login.tsx
+++ school-library-app/src/pages/Authentication/Login.tsx
@@ -1,9 +1,17 @@
-import { Paper, Title, TextInput, Button, Anchor, Text } from "@mantine/core";
+import {
+  Paper,
+  Title,
+  TextInput,
+  Button,
+  Anchor,
+  Text,
+  Grid,
+  Image,
+  Box,
+} from "@mantine/core";
 import classes from "./login.module.css";
 import Form from "@components/Form/Form";
 import { useForm } from "react-hook-form";
-import { login } from "./services/auth.service";
-import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import useLogin from "./hooks/useLogin";
@@ -15,8 +23,6 @@ const Login = () => {
 
   const { loginUser, isPending } = useLogin();
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit = async (data: Record<string, any>) => {
     try {
@@ -26,82 +32,124 @@ const Login = () => {
       toast.error(
         `Oops! It seems the credentials you entered are invalid. Please double-check your username and password and try again.`
       );
-    } finally {
-      setIsLoading(false);
     }
   };
   return (
-    <div className={classes.wrapper}>
-      <Paper className={classes.form} radius={0} p={30}>
-        <Title order={2} className={classes.title} ta="center" mt="xl" mb={50}>
-          Welcome back to BCC OPAC
-        </Title>
+    <Box
+      style={{
+        overflow: "hidden",
+      }}
+    >
+      <Grid>
+        <Grid.Col
+          span={{
+            base: 12,
+            lg: 8,
+            md: 8,
+          }}
+          visibleFrom="md"
+        >
+          <Image src={"/images/login_bcc.png"} mah={"100vh"} visibleFrom="lg" />
 
-        {/* Login */}
-        <Form onSubmit={form.handleSubmit(onSubmit)}>
-          <Form.Box>
-            <Form.Grid>
-              <Form.Col>
-                <TextInput
-                  label="Email address"
-                  placeholder="hello@gmail.com"
-                  size="md"
-                  {...form.register("email", {
-                    required: "This field is required!",
-                  })}
-                  withAsterisk
-                  withErrorStyles={
-                    form.formState.errors.email?.message ? true : false
-                  }
-                  error={<>{form.formState.errors.email?.message}</>}
-                />
-              </Form.Col>
-              <Form.Col>
-                <TextInput
-                  label="Password"
-                  placeholder="Your password"
-                  mt="md"
-                  size="md"
-                  type="password"
-                  withAsterisk
-                  withErrorStyles={
-                    form.formState.errors.password?.message ? true : false
-                  }
-                  error={<>{form.formState.errors.password?.message}</>}
-                  {...form.register("password", {
-                    required: "This field is required!",
-                  })}
-                />
-              </Form.Col>
-            </Form.Grid>
-          </Form.Box>
-          <Button
-            fullWidth
-            loading={isPending}
-            mt="xl"
-            size="md"
-            type="submit"
-            color="#5C0505"
-          >
-            Login
-          </Button>
-        </Form>
+          <Image src={"/images/login_bcc.png"} mih={"100vh"} hiddenFrom="lg" />
+        </Grid.Col>
 
-        {/* end of login */}
+        <Grid.Col
+          span={{
+            base: 12,
+            lg: 4,
+            md: 4,
+          }}
+        >
+          <Paper className={classes.form} radius={0} p={30}>
+            <Title
+              order={2}
+              className={classes.title}
+              ta="center"
+              mt="xl"
+              mb={50}
+              ff={"Montserrat"}
+              fw={500}
+            >
+              <span className={classes.highlight}>
+                Online Public Access Catalog <br />
+              </span>
+              <Text span c="dimmed">
+                Binangonan Catholic College
+              </Text>
+            </Title>
 
-        {/* {isError && <Text c={"red"}>Invalid Email and Password</Text>} */}
+            {/* Login */}
+            <Form onSubmit={form.handleSubmit(onSubmit)}>
+              <Form.Box>
+                <Form.Grid>
+                  <Form.Col>
+                    <TextInput
+                      label="Email address"
+                      placeholder="hello@gmail.com"
+                      size="md"
+                      {...form.register("email", {
+                        required: "This field is required!",
+                      })}
+                      withAsterisk
+                      withErrorStyles={
+                        form.formState.errors.email?.message ? true : false
+                      }
+                      error={<>{form.formState.errors.email?.message}</>}
+                    />
+                  </Form.Col>
+                  <Form.Col>
+                    <TextInput
+                      label="Password"
+                      placeholder="Your password"
+                      mt="md"
+                      size="md"
+                      type="password"
+                      withAsterisk
+                      withErrorStyles={
+                        form.formState.errors.password?.message ? true : false
+                      }
+                      error={<>{form.formState.errors.password?.message}</>}
+                      {...form.register("password", {
+                        required: "This field is required!",
+                      })}
+                    />
+                  </Form.Col>
+                </Form.Grid>
+              </Form.Box>
+              <Button
+                fullWidth
+                loading={isPending}
+                mt="xl"
+                size="md"
+                type="submit"
+                color="#5C0505"
+              >
+                Login
+              </Button>
+            </Form>
 
-        <Text ta="center" mt="md">
-          <Anchor<"a"> fw={700} onClick={() => navigate("/forget-password")}>
-            Forget your password?
-          </Anchor>
-        </Text>
+            {/* end of login */}
 
-        <Text ta="center" mt="md">
-          <Link to={"/library"}>Go back to Library</Link>
-        </Text>
-      </Paper>
-    </div>
+            {/* {isError && <Text c={"red"}>Invalid Email and Password</Text>} */}
+
+            <Text ta="center" mt="md">
+              <Anchor<"a">
+                fw={700}
+                onClick={() => navigate("/forget-password")}
+              >
+                Forget your password?
+              </Anchor>
+            </Text>
+
+            <Text ta="center" mt="md">
+              <Link to={"/library"}>Go back to Library</Link>
+            </Text>
+            <Image src={"/images/bg.png"} />
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </Box>
   );
 };
 export default Login;
